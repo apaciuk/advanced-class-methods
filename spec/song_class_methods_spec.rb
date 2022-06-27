3e3136f1b9eb493fe13d91be981c0170bb8ef280
Paul Anthony McGowan <xhostcom@gmail.com>
2022-06-27 08:17:31 +0100

    Update

--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 describe 'Song Class methods' do
+  let(:song) { described_class.new }
   describe '.create' do 
   it 'instantiates and saves the song, and returns the song created' do 
-  song = Song.create(name: 'My Song')
+  #song = Song.create(name: 'My Song')
   expect(song).to be_a(Song)
   expect(Song.all).to include([song])
-  end
-  describe "::all" do
-    it "returns an array of all songs" do
-      song1 = Song.new
-      song1.save
-      song2 = Song.new
-      song2.save
+  end 
+end
 
-      expect(Song.all).to eq([song1, song2])
+   describe 'new by name' do 
+    it 'instantiates and saves the song with a name property' do 
+
     end
   end
 end

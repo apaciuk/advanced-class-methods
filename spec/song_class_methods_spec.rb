9d34ff602a6b64e33de27e53463d956b1a45797a
Paul Anthony McGowan <xhostcom@gmail.com>
2022-06-27 09:10:03 +0100

    adjust spec

--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
+#include '../lib/song'
 
-describe 'Song Class methods' do
-  let(:song) { described_class.new }
+RSpec.describe 'Song class methods' do
+  let(:song) do
+    Song.new(ApplicationRecord) do
+    end
+    end
   describe '.create' do 
   it 'instantiates and saves the song, and returns the song created' do 
   #song = Song.create(name: 'My Song')
   expect(song).to be_a(Song)
   expect(Song.all).to include([song])
   end 
-end
+  end
+  #let(:song) { described_class.new }
 
    describe 'new by name' do 
     it 'instantiates and saves the song with a name property' do 
